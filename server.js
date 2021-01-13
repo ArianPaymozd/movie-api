b4bdcb06e946b2648a0e7370adc24a47ed35b564
ArianPaymozd <arianyazdi@yahoo.com>
2021-01-13 09:53:20 -0800

    prepared for production

--- server.js
+++ server.js
@@ -6,11 +6,12 @@ const helmet = require('helmet')
 const cors = require('cors')
 
 const MOVIES = require('./movies.json')
+const app = require('../express-boilerplate/src/app')
 
-console.log(process.env.API_TOKEN)
 app = express()
 
-app.use(morgan('dev'))
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
+app.use(morgan(morganSetting))
 app.use(helmet())
 app.use(cors())
 
@@ -18,8 +19,6 @@ app.use(cors())
 app.use(function validateBearerToken(req, res, next) {
     const authToken = req.get('Authorization')
     const apiToken = process.env.API_TOKEN
-
-    console.log('validate bearer')
 
     if (!authToken || authToken.split(' ')[1] !== apiToken) {
         return res.status(401).json({error: 'Unauthorized request'})
@@ -50,7 +49,17 @@ app.get('/movies', function handleGetMovies(req, res) {
     res.json(response)
 })
 
-const PORT = 8000
+app.use((error, req, res, next) => {
+    let response
+    if (process.env.NODE_ENV === 'production') {
+        response = {error: {message: 'Server error'}}
+    }else {
+        response = {error}
+    }
+    res.status(500).json(response)
+})
+
+const PORT = process.env.PORT || 8000
 app.listen(PORT, () => {
     console.log(`Server is listening on https://localhost:${PORT}`)
 })
